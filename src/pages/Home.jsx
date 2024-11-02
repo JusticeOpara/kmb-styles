5c1c61cdaff7e6bec17924ae2b328374b1aafa3f
Justice Opara <justiceopara33@gmail.com>
2024-11-02 22:07:05 +0100

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+
+import Hero from '../components/Hero';
+import LatestCollection from '../components/LatestCollection';
+import NewsLetterBox from '../components/NewsLetterBox';
+import OurPolicies from '../components/OurPolicies';
+import BestSeller from '../components/bestseller';
+const Home = () => {
+  return (
+    <div>
+      <Hero />
+      <LatestCollection />
+      <BestSeller />
+      <OurPolicies />
+      <NewsLetterBox />
+    </div>
+  );
+};
+
+export default Home;
